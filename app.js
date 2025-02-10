d2c83f1b760d74d6a489da98c93b9dc31fb2dd5d
Jackson Roque <jackson.roque@protonmail.com>
2025-02-10 20:55:12 -0300

    criação da função que gera números aleatórios

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 function sortear(){
-    let quantidade = document.getElementById('quantidade').value;
-    let de = document.getElementById('de').value;
-    let ate = document.getElementById('ate').value;
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
 
-    alert(`Quantidade: ${quantidade}`);
-    alert(`Do número: ${de}`);
-    alert(`Até o número: ${ate}`);
+    let sorteados = [];
+    let numero;
+
+    for (let i = 0; i < quantidade; i++){
+        numero = obeterNumeroAleatorio(de, ate);
+        sorteados.push(numero);
+    }
+
+    alert(sorteados);
+
+}
+
+function obeterNumeroAleatorio(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+
 }
